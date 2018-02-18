475d195453bcaa7d20a5a7d1b4a2a1f1da18be58
Test <test@Tests-MacBook-Air.local>
2018-02-18 19:39:56 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,38 @@
+ymaps.ready(init);
+    var myMap,
+        myPlacemark1;
+        myPlacemark2;
+
+    function init(){
+        myMap = new ymaps.Map("map", {
+            center: [55.75452093, 37.62505870],
+            controls: ['smallMapDefaultSet'],
+            zoom: 13
+        });
+
+        myPlacemark1 = new ymaps.Placemark([55.75048523, 37.60537096], {
+          balloonContentHeader: 'yellowvibecoffee',
+          balloonContentBody: 'yellowvibecoffee',
+          balloonContentFooter: 'yellowvibecoffee',
+          hintContent: 'yellowvibecoffee'
+        },{
+        iconLayout: 'default#image',
+        iconImageHref: 'img/marker.png',
+        iconImageSize: [36, 40],
+        iconImageOffset: [-3, -42]
+        });
+        myPlacemark2 = new ymaps.Placemark([55.75864028, 37.65915550], {
+          balloonContentHeader: 'yellowvibecoffee',
+          balloonContentBody: 'yellowvibecoffee',
+          balloonContentFooter: 'yellowvibecoffee',
+          hintContent: 'yellowvibecoffee'
+        },{
+        iconLayout: 'default#image',
+        iconImageHref: 'img/marker.png',
+        iconImageSize: [36, 40],
+        iconImageOffset: [-3, -42]
+        });
+
+          myMap.geoObjects.add(myPlacemark1);
+          myMap.geoObjects.add(myPlacemark2);
+    }
